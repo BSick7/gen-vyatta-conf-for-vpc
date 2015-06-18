0735a921f7c6ece606daf4ea96992cd30543b7c7
BSick7 <brad.sickles@gmail.com>
2015-06-18 14:34:02 -0400

    Added command line arguments parsing.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var fs = require('fs'),
+var stdio = require('stdio'),
+    fs = require('fs'),
     path = require('path'),
     input = require('./input'),
     lib = require('./lib');
 
-var inputs = input.get();
+var ops = stdio.getopt({
+    "local_subnet": {key: "l", mandatory: true, description: "Vyatta Local subnet"},
+    "remote_subnet": {key: "r", mandatory: true, description: "Vyatta Remote subnet"},
+    "interface": {key: "f", description: "Vyatta Interface device"},
+    "input": {key: "i", description: "Input filepath"},
+    "output": {key: "o", description: "Output filepath"}
+});
+
+var inputs = input.get(ops);
 var result = lib.generate(inputs);
 
-var outDir = path.join(__dirname, 'out');
+var outDir = ops.output || path.join(__dirname, 'out');
 if (!fs.existsSync(outDir))
     fs.mkdirSync(outDir);
 fs.writeFileSync(path.join(outDir, 'vyatta.config'), result.config);
--- input/index.js
+++ input/index.js
@@ -1,11 +1,11 @@
 var fs = require('fs'),
     path = require('path');
 
-exports.get = function () {
+exports.get = function (ops) {
     return {
-        interface: "eth0",
-        local_subnet: "10.208.231.70/19",
-        remote_subnet: "10.0.0.0/16",
-        config_from_vpc: fs.readFileSync(path.join(__dirname, "config.txt"), { encoding: 'utf-8' })
+        interface: ops.interface || "eth0",
+        local_subnet: ops.local_subnet,// "10.208.231.70/19",
+        remote_subnet: ops.remote_subnet,// "10.0.0.0/16",
+        config_from_vpc: fs.readFileSync(ops.input || path.join(__dirname, "config.txt"), {encoding: 'utf-8'})
     };
 };
