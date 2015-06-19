33aeed7748e8ba4566285778c0019fb46863b381
BSick7 <brad.sickles@gmail.com>
2015-06-19 11:35:17 -0400

    Adding vpc remote subnet network to vyatta config.

--- input/index.js
+++ input/index.js
@@ -4,8 +4,8 @@ var fs = require('fs'),
 exports.get = function (ops) {
     return {
         interface: ops.interface || "eth0",
-        local_subnet: ops.local_subnet,// "10.208.231.70/19",
-        remote_subnet: ops.remote_subnet,// "10.0.0.0/16",
+        local_subnet: ops.local_subnet,
+        remote_subnet: ops.remote_subnet,
         config_from_vpc: fs.readFileSync(ops.input || path.join(__dirname, "config.txt"), {encoding: 'utf-8'})
     };
 };
